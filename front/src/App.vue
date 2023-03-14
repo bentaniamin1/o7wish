b196303188b71647f76c04547e7f1adc5856a028
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2023-03-14 09:54:56 +0100

    authentication

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <AuthenticationView />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import AuthenticationView from "@/views/AuthenticationView";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AuthenticationView
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/styles/form_style.scss";
+@import "assets/styles/reset.css";
+@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@200&display=swap');
+
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  font-family: 'Readex Pro', sans-serif;
+  font-weight: bold;
+  font-size: 1.5rem;
+}
+
+button {
+  background: #B3145E;
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  font-family: 'Readex Pro', sans-serif;
+  font-weight: bold;
+  font-size: 1.125rem;
+}
+button:hover {
+  cursor: pointer;
+}
+input {
+  font-family: 'Readex Pro', sans-serif;
+  font-weight: bold;
+  font-size: 1.125rem;
 }
 </style>
---
+++ front/src/components/Authentication/RegisterForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form action="" method="get" class="">
+    <div class="form_label_groupe">
+      <input type="text" name="name" id="name" placeholder="Votre nom" required>
+    </div>
+    <div class="form_label_groupe">
+      <input type="password" name="password" id="password" placeholder="Votre mot de passe" required>
+    </div>
+    <div class="form_label_groupe">
+      <input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirmez votre mot de passe"
+             required>
+    </div>
+    <div class="form_label_groupe">
+      <button type="submit" value="">Inscription</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm"
+}
+</script>
+
+<style scoped>
+
+</style>
