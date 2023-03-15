8e87ba5e46a2c91d63e74052a9b2d5de826b5f1e
ArkhayDS <raphael.ds@outlook.fr>
2023-03-15 11:03:17 +0100

    updating controllers / yaml configs and db connexion

--- back/src/Controller/UserController.php
+++ back/src/Controller/UserController.php
@@ -8,11 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
+    
+    public function loginUI()
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        
+    }
+
+    public function registerUI()
+    {
+        
     }
 }
