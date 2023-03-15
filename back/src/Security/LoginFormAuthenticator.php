9f7c99a14523b3ed75734073f33c9902441d246e
IkuroMaira <gwen.bussac@gmail.com>
2023-03-15 09:13:58 +0100

    add ApiController

--- back/src/Security/LoginFormAuthenticator.php
+++ back/src/Security/LoginFormAuthenticator.php
@@ -49,6 +49,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // Redirection
     }
 
     protected function getLoginUrl(Request $request): string
