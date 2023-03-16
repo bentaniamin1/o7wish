5d2f5f122117ac7acca439767f12c6d5a31cec3d
ArkhayDS <raphael.ds@outlook.fr>
2023-03-16 12:23:19 +0100

    merging & debugging Amin branch

--- back/src/Service/JWTHelper.php
+++ back/src/Service/JWTHelper.php
@@ -1,26 +1,26 @@
 <?php
-
-namespace App\Service;
-
-// Exit if accessed directly
-defined( 'ABSPATH' ) || exit;
-
-if ( !class_exists( 'JWTHelper' ) ) {
-
-    /**
-     * Class JWTHelper
-     */
-    class JWTHelper {
-
-        /**
-         * JWTHelper constructor.
-         */
-        public function __construct() {
-            // Do something
-        }
-
-    }
-
-    // Instantiate
-    new JWTHelper();
-}
+//
+//namespace App\Service;
+//
+//// Exit if accessed directly
+//defined( 'ABSPATH' ) || exit;
+//
+//if ( !class_exists( 'JWTHelper' ) ) {
+//
+//    /**
+//     * Class JWTHelper
+//     */
+//    class JWTHelper {
+//
+//        /**
+//         * JWTHelper constructor.
+//         */
+//        public function __construct() {
+//            // Do something
+//        }
+//
+//    }
+//
+//    // Instantiate
+//    new JWTHelper();
+//}
